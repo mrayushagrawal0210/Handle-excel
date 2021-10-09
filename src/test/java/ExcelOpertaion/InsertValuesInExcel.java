809ccda1d2018b0ae2ae57a9de456d11c04326fc
Prajjawal Kansal <43197101+prajjawal98@users.noreply.github.com>
2021-10-09 17:42:35 +0530

    Update InsertValuesInExcel.java

--- src/test/java/ExcelOpertaion/InsertValuesInExcel.java
+++ src/test/java/ExcelOpertaion/InsertValuesInExcel.java
@@ -6,13 +6,12 @@ import com.codoid.products.fillo.Fillo;
 import org.testng.annotations.Test;
 
 public class InsertValuesInExcel {
-
 	private String query;
 	private String filePath;
 
 	@Test
 	public void test1() {
-		query = "INSERT INTO \"Info - Sheet3\"(StudentID,\"First Name\",\"Last Name\",ProgrammingLanguage,\"Age\") VALUES(3,'abhi','garg','C',22)";
+		query = "INSERT INTO \"Info - Sheet3\"(StudentID,\"First Name\",\"Last Name\",ProgrammingLanguage,\"Age\") VALUES(4,'ayush','agrawal','k',24)";
 		filePath = "./files/SampleExcelFile1.xlsx";
 		try {
 			Fillo fillo = new Fillo();
